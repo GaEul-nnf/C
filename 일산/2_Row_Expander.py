3da1bb260c2ca2170b09ad5d8222338d527027a9
Jeong_GaEul <nnfgaeul@gmail.com>
2024-12-05 13:46:51 +0900

    Delete 일산 directory

--- 일산/2_Row_Expander.py
+++
@@ -1,6 +0,0 @@
-# 한개의 행을 6개로 나눔
-import pandas as pd
-
-df = pd.read_csv('E:\\업무지시\\백석1동_기온_20230111_20241031.csv')
-df_repeated = df.loc[df.index.repeat(6)].reset_index(drop=True)
-df_repeated.to_csv('E:\\업무지시\\six_백석1동_기온_20230111_20241031.csv', index=False)
